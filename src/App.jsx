11cc5424b70296e090396b358bad96092862da8a
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2023-01-30 16:17:22 -0300

    Sidebar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import { Sidebar } from "./components/SideBar";
+
+import styles from "./App.module.css";
 
 import "./global.css";
 
@@ -8,14 +11,19 @@ export function App() {
     <div>
       <Header />
 
-      <Post
-        author="Andre Barbosa"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit accusantium autem eligendi neque numquam consequatur aspernatur veniam? Consequuntur ad totam tempore, necessitatibus, dolores repellat inventore iure voluptas iusto libero explicabo."
-      />
-      <Post
-        author="Karol Matias"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit accusantium autem eligendi neque numquam consequatur aspernatur veniam? Consequuntur ad totam tempore, necessitatibus, dolores repellat inventore iure voluptas iusto libero explicabo."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Andre Barbosa"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit accusantium autem eligendi neque numquam consequatur aspernatur veniam? Consequuntur ad totam tempore, necessitatibus, dolores repellat inventore iure voluptas iusto libero explicabo."
+          />
+          <Post
+            author="Karol Matias"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit accusantium autem eligendi neque numquam consequatur aspernatur veniam? Consequuntur ad totam tempore, necessitatibus, dolores repellat inventore iure voluptas iusto libero explicabo."
+          />
+        </main>
+      </div>
     </div>
   );
 }
