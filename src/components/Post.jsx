bdade4db10cdd8ea7210057521d65852b01428ff
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2023-01-31 15:42:40 -0300

    Post component created

---
+++ src/components/Post.jsx
@@ -0,0 +1,43 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/AOBarbosa.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>André Barbosa</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="31 de Janeiro de 2023" dateTime="2023-01-31 14:52:15">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          {" "}
+          <a href="">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
