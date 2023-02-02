7341acc91626a36eafbd9531de3d3681dbaead1b
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2023-02-01 22:52:45 -0300

    Comments can be added interactively with useState

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,13 @@
 import { format, formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { useState } from "react";
 
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
+
+const comments = [1, 2]; // temporario
 
 /**
  * Para nao ficar repetindo o nome props nas tags, podemos fazer a desetruturaçao
@@ -22,6 +25,14 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   });
+
+  const [comments, setComments] = useState([1, 2]);
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, comments.lenght + 1]);
+  }
 
   return (
     <article className={styles.post}>
@@ -56,7 +67,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Leave your feedback</strong>
 
         <textarea placeholder="Leave a comment" />
@@ -67,9 +78,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment />;
+        })}
       </div>
     </article>
   );
